13257bcef648ba7d559852bd5226804e53cf6d27
Sarra Mousselmal <101995037+SarraMsl@users.noreply.github.com>
2023-05-29 21:52:47 +0200

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,9 +1,9 @@
  <?php  
 
-$sname = "eu-cdbr-west-03.cleardb.net";
-$uname = "bb0008fa13895e";
-$password = "855e553e";
-$db_name = "heroku_86d27cbc62bff9e";
+$sname = "";
+$uname = "";
+$password = "";
+$db_name = "";
 $conn  = mysqli_connect($sname, $uname, $password, $db_name);
 
 if (!$conn) {
